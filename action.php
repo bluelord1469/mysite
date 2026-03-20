095518808eaac037b0a4df58d9dd783807e875e8
Detsel <illhacku.hah@gmail.com>
2026-03-20 13:46:02 +0300

    Добавили валидацию полей

--- action.php
+++ action.php
@@ -5,6 +5,17 @@ $gender = isset($_POST['gender']) ? $_POST['gender'] : '';
 $password = isset($_POST['password']) ? $_POST['password'] : '';
 $password_confirm = isset($_POST['password_confirm']) ? $_POST['password_confirm'] : '';
 $terms = isset($_POST['terms']) ? 'Принято' : 'Не принято';
+
+$error_message = '';
+
+if ($username == 'detsel' and $password == '123'){ require('calculator.php'); }
+
+if (empty($email)) {
+$error_message = "Поле Email не заполнено";
+} 
+elseif (empty($password)) {
+$error_message = "Поле Пароль не заполнено";
+
 ?>
     <!DOCTYPE html>
     <html lang="ru">
@@ -16,7 +27,11 @@ $terms = isset($_POST['terms']) ? 'Принято' : 'Не принято';
     </head>
     <body>
         <div class="area">
+            <?php if (!empty($error_message)): ?>
+            <p><?php echo $error_message; ?></p>
+        <?php else: ?>
             <h2>Здравствуйте, <?php echo $username; ?>!</h2>
+        <?php endif; ?>
         </div>
     </body>
     </html>
