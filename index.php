79c5ac0e29ab891f96aa2fce72db3f355df3d5aa
Detsel <illhacku.hah@gmail.com>
2026-03-25 16:11:35 +0300

    Удалили файл world.txt

--- index.php
+++ index.php
@@ -1,12 +1,12 @@
 <?php
-$file = fopen("test.txt", 'w') or die("Не удалось открыть файл");
+$file = fopen("test.txt", 'w') or die("Не удалось открыть файл<br>");
 $str = "Привет, мир!";
 fputs($file, $str);
 
 $text = htmlentities(file_get_contents("test.txt"));
 echo $text . "<br>";
 
-rename("test.txt", "mir.txt") or die("Ошибка переименования файла");
+rename("test.txt", "mir.txt") or die("Ошибка переименования файла<br>");
 
 mkdir("/var/www/mysite.ru/folder", 0700);
 rename("mir.txt", "folder/mir.txt") or die("Ошибка перемещения файла в папку");
@@ -20,6 +20,9 @@ $fsize = filesize("folder/world.txt");
 echo $fsize . " байт<br>";
 echo $fsize/1024/1024 . " мегабайт<br>";
 echo $fsize/1024/1024/1024 . " гигабайт<br>";
+
+if (unlink("folder/world.txt")) echo "Файл удален<br>";
+else echo "Ошибка при удалении файла<br>";
 
 fclose($file);
 
