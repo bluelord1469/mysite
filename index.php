54806ef735c07c3d4be87350ef87806fb245a79f
Detsel <illhacku.hah@gmail.com>
2026-03-25 16:07:26 +0300

    Вывели размер файла world в б, Мб и Гб

--- index.php
+++ index.php
@@ -12,8 +12,14 @@ mkdir("/var/www/mysite.ru/folder", 0700);
 rename("mir.txt", "folder/mir.txt") or die("Ошибка перемещения файла в папку");
 
 if(copy("/var/www/mysite.ru/folder/mir.txt", "/var/www/mysite.ru/folder/world.txt"))
-echo "Копия файла создана";
+echo "Копия файла создана<br>";
 else echo "Ошибка копирования файла";
+
+$fsize = filesize("folder/world.txt");
+
+echo $fsize . " байт<br>";
+echo $fsize/1024/1024 . " мегабайт<br>";
+echo $fsize/1024/1024/1024 . " гигабайт<br>";
 
 fclose($file);
 
