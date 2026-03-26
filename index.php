a77673b870d2126888f7f8a9db1471259ea21ce5
Detsel <illhacku.hah@gmail.com>
2026-03-26 15:32:13 +0300

    Написали обработчик для обработки ошибки деления на 0 и сохранили ошибку в файл

--- index.php
+++ index.php
@@ -1,20 +1,40 @@
 <?php
-function checkOpenFile($filename, $rights)
+//function checkOpenFile($filename, $rights)
+//{
+//if (!file_exists($filename)) throw new Exception("Файл не существует");
+//$file = fopen($filename, $rights);
+//return $file;
+//}
+
+//try
+//{
+//$file = checkOpenFile("file.txt", "r");
+//echo "Файл открыт";
+//fclose($file);
+//}
+//catch (Exception $ex)
+//{
+//echo $ex->getMessage();
+//}
+
+function checkDivisionByZero($num1, $num2)
 {
-if (!file_exists($filename)) throw new Exception("Файл не существует");
-$file = fopen($filename, $rights);
-return $file;
+if ($num2 == 0) throw new Exception("Деление на ноль невозможно!");
+return $num1/$num2;
 }
 
 try
 {
-$file = checkOpenFile("file.txt", "r");
-echo "Файл открыт";
-fclose($file);
+$res = checkDivisionByZero(13, 0);
+echo $res . "<br>";
 }
 catch (Exception $ex)
 {
 echo $ex->getMessage();
+$err = $ex->getMessage();
+$errFile = fopen("errFile.txt", "a");
+fputs($errFile, $err);
+fclose($errFile);
 }
 
 ?>
