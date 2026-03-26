2fc43937bbd1c649b12b2df92928518d0b809d3c
Detsel <illhacku.hah@gmail.com>
2026-03-26 23:08:15 +0300

    Произвели + и - к данной дате

--- index.php
+++ index.php
@@ -120,7 +120,14 @@
 //</html>
 
 
-$date = '2026-03-26';
-echo date('d-m-Y', strtotime($date));
+//$date = '2026-03-26';
+//echo date('d-m-Y', strtotime($date));
+
+$timestamp = strtotime('2000-02-03');
+
+echo date('Y.m.d', strtotime('+2 days', $timestamp)) . "<br>";
+echo date('Y.m.d', strtotime('+1 month +3 days', $timestamp)) . "<br>";
+echo date('Y.m.d', strtotime('+1 year', $timestamp)) . "<br>";
+echo date('Y.m.d', strtotime('-3 days', $timestamp)) . "<br><br>";
 
 ?>
