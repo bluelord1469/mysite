736e0ff6ee37dae6d79e36f1be7dad2ff591564a
Detsel <illhacku.hah@gmail.com>
2026-04-02 11:21:01 +0300

    Вывели сумму возрастов и зарплат

--- index.php
+++ index.php
@@ -9,7 +9,7 @@ public function __construct($name, $age, $salary)
 {
 $this->name = $name;
 $this->age = $age;
-$this->salary = $age;
+$this->salary = $salary;
 }
 
 }
@@ -19,5 +19,9 @@ $emp2 = new Employee("David", 17, 80000);
 
 echo "name: " . $emp1->name . " age: " . $emp1->age . " salary: " . $emp1->salary . "<br>";
 print_r($emp2);
+echo "<br>";
+
+echo $emp1->age + $emp2->age . "<br>";
+echo $emp1->salary + $emp2->salary . "<br>";
 
 ?>
