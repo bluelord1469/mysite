ed3b651a3384f57ee25fb947cb0dc9e594b7c637
Detsel <illhacku.hah@gmail.com>
2026-04-02 11:43:31 +0300

    Создали метод получения суммы зарплат сотрудников

--- index.php
+++ index.php
@@ -18,10 +18,19 @@ public function getAge() { return $this->age; }
 
 public function getSalary() { return $this->salary; }
 
+public function getSalarySum($emps) 
+{
+$sum = 0;
+foreach($emps as $emp) { $sum += $emp->getSalary(); }
+return $sum;
+}
+
 }
 
 $emp1 = new Employee("Detsel", 18, 100000);
 $emp2 = new Employee("David", 17, 80000);
+
+$emps = [$emp1, $emp2];
 
 echo "name: " . $emp1->name . " age: " . $emp1->age . " salary: " . $emp1->salary . "<br>";
 print_r($emp2);
@@ -33,5 +42,7 @@ echo $emp1->salary + $emp2->salary . "<br>";
 echo $emp1->getName() . "<br>";
 echo $emp2->getAge() . "<br>";
 echo $emp2->getSalary() . "<br>";
+
+echo $emp1->getSalarySum($emps);
 
 ?>
